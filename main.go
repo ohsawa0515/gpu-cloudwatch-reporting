48ddc23f54da4a2a7234912bf2a2732bf7cb7ef8
Shuichi Ohsawa <ohsawa0515@gmail.com>
2020-10-12 20:23:05 +0900

    output environment variables

--- main.go
+++ main.go
@@ -19,6 +19,12 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	log.Printf("REGION: %s", cwConfig.Region)
+	log.Printf("NAMESPACE: %s", cwConfig.NameSpace)
+	log.Printf("SEND_INTERVAL_SECOND: %s", nvidiaConfig.SendIntervalSecond)
+	log.Printf("COLLECT_INTERVAL_SECOND: %s", nvidiaConfig.CollectIntervalSecond)
+
 	ctx := context.Background()
 	if err := client.Run(ctx); err != nil {
 		log.Fatal(err)
